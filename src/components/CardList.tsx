7c59c15efbc6bd0d205f36f7ecbbb88a3bcd5430
gomisha <misha@gomisha.com>
2018-07-17 19:46:31 -0400

    minor clean up

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -5,7 +5,15 @@ import ICardListProps from "./ICardListProps";
 
 const CardList = (props: ICardListProps) => {
     const cardList = props.robots.map((current, i) => {
-        return <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email} username={robots[i].username}/>;
+        return (
+            <Card
+                key={robots[i].id}
+                id={robots[i].id}
+                name={robots[i].name}
+                email={robots[i].email}
+                username={robots[i].username}
+            />
+        );
     });
     return (
         <div>
