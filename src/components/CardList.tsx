6750c2b53b4ee6f43ed8344b94f60a810eef94f7
gomisha <misha@gomisha.com>
2018-07-17 19:35:43 -0400

    CardList reads all robots

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -4,17 +4,12 @@ import Card from "./Card";
 import ICardListProps from "./ICardListProps";
 
 const CardList = (props: ICardListProps) => {
-    const cardList = props.robots.map(current => {
-        return <Card key={robots[0].id} id={robots[0].id} name={robots[0].name} email={robots[0].email} username={robots[0].username}/>;
+    const cardList = props.robots.map((current, i) => {
+        return <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email} username={robots[i].username}/>;
     });
     return (
         <div>
-            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} username={robots[0].username}/>
-            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} username={robots[0].username}/>
-            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} username={robots[1].username}/>
-            <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} username={robots[2].username}/>
-            <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} username={robots[3].username}/>
-            <Card id={robots[4].id} name={robots[4].name} email={robots[4].email} username={robots[4].username}/>
+            {cardList}
         </div>
     );
 };
