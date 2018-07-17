2d26856ab633b34f4cc8c5589c4d9fe70b946ec1
gomisha <misha@gomisha.com>
2018-07-17 18:06:29 -0400

    added CardList component

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import "tachyons";
-// import App from './App';
-// import Hello2 from "./components/Hello2";
-// import Hello3 from "./components/Hello3";
-// import './index.css';
-import Card from "./components/card/Card";
+import CardList from "./components/CardList";
+// import { robots } from "./data/robots";
 import registerServiceWorker from './registerServiceWorker';
-import { robots } from "./robots";
-
+// import CardList from './components/CardList';
 
 ReactDOM.render(
     <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+        <CardList/>
     </div>
     ,
-//    <Hello3 greeting={'hello' + 'react ninja222'}/>,
-//  <Hello2 name="TypeScript" enthusiasmLevel={3} />,
-//   <App/>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
