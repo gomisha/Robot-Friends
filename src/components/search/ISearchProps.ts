306cf01119f9b8b6862ee34f103cc466cc28d8f4
gomisha <misha@gomisha.com>
2018-07-18 11:14:19 -0400

    minor lint cleanup

--- src/components/search/ISearchProps.ts
+++ src/components/search/ISearchProps.ts
@@ -1,9 +1,7 @@
 interface ISearchProps {
     searchField: string,
-    //value is a (callback) function that takes an event parameter
-    searchChange: {
-        (event: any): void
-    }
+    // value is a (callback) function that takes an event parameter
+    searchChange: (event: any) => void
 }
 
 export default ISearchProps;
