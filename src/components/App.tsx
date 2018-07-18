4f6ffde737c9ed198ff94e9539c03157d4dfe5cf
gomisha <misha@gomisha.com>
2018-07-18 07:44:40 -0400

    changed App component to class to hold state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,14 +4,16 @@ import robots from "../data/robots";
 import CardList from "./CardList";
 import SearchBox from './SearchBox';
 
-const App = () => {
-    return(
-        <div className="tc">
-            <h1>Robo Friends</h1>
-            <SearchBox/>
-            <CardList robots={robots}/>
-        </div>
-    );
-};
+class App extends React.Component {
+    public render() {
+        return(
+            <div className="tc">
+                <h1>Robo Friends</h1>
+                <SearchBox/>
+                <CardList robots={robots}/>
+            </div>
+        );
+    }
+}
 
 export default App;
